1eff66e5a6102deeb97c6445804d1de699e37efb
Pablo DC <pablo@datakitchen.io>
2018-06-20 09:00:16 -0400

    Version 1.0.77.2

--- DKCloudCommand/modules/githash.py
+++ DKCloudCommand/modules/githash.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import codecs
+
 from sys import argv
 from hashlib import sha1
 from cStringIO import StringIO
+
 
 class githash(object):
     def __init__(self):
@@ -26,11 +29,12 @@ def githash_data(data):
     return h.hexdigest()
 
 
-def githash_fileobj(fileobj):
-    return githash_data(fileobj.read())
+def githash_by_file_name(file_name):
+    with codecs.open(file_name, 'r', encoding='utf-8') as f:
+        file_contents = f.read()
+        return githash_data(file_contents.encode('utf-8'))
 
 
 if __name__ == '__main__':
     for filename in argv[1:]:
-        fileobj = file(filename)
-        print(githash_fileobj(fileobj))
+        print(githash_by_file_name(filename))
